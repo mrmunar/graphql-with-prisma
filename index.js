027867b086289022216e61913dd229ea963b0775
Migs Munar <miguel.munar@flexisourceit.com.au>
2020-05-07 09:27:20 +0800

    Added graphql-yoga boilerplate

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
+const { GraphQLServer } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-async function main() {
-    await prisma.users.create({
-        data: {
-            firstname: 'Alice',
-            lastname: 'Cooper',
-        }
-    })
+const typeDefs = `
+  type Query {
+    testCreateUser: String
+    testGetUsers: [User!]!
+  }
 
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-            profile: true
-        },
-    })
-    console.dir(allUsers, { depth: null })
+  type User {
+    firstname: String
+    id: ID
+    lastname: String
+    photo_url: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    testCreateUser: async () => {
+        await prisma.users.create({
+            data: {
+                firstname: 'Alice',
+                lastname: 'Cooper',
+                photo_url: ''
+            }
+        })
+        
+        console.log('user created')
+        return 'user created'
+    },
+    testGetUsers: async () => {
+        const allUsers = await prisma.users.findMany()
+
+        return allUsers
+    }
+  }
 }
 
-main()
-    .catch(e => {
-        console.error(e.message)
-    })
-    .finally(async () => {
-        await prisma.disconnect()
-    })
+const server = new GraphQLServer({ 
+    typeDefs, 
+    resolvers
+})
+
+server.start(() => console.log('Server started on port 4000'))
