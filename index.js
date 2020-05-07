3500df1e5880154d275c3c21bbfe9e52e5051d08
Migs Munar <miguel.munar@flexisourceit.com.au>
2020-05-07 15:08:25 +0800

    Added second test model

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const typeDefs = `
   type Query {
     testCreateUser: User!
     testGetUsers: [User!]!
+    testCreatePost: Post!
+    testGetPosts: [Post!]!
   }
 
   type User {
@@ -15,28 +17,48 @@ const typeDefs = `
     lastname: String
     photo_url: String
   }
+
+  type Post {
+    id: ID
+    title: String
+    content: String
+  }
 `;
 
 const resolvers = {
-  Query: {
-    testCreateUser: async () => {
-        return await prisma.users.create({
-            data: {
-                firstname: 'Alice',
-                lastname: 'Cooper',
-                photo_url: ''
-            }
-        })
-    },
-    testGetUsers: async () => {
-        return await prisma.users.findMany()
+    Query: {
+        testCreateUser: async (parent, args, ctx) => {
+            return await ctx.prisma.users.create({
+                data: {
+                    firstname: 'Alice',
+                    lastname: 'Cooper',
+                    photo_url: ''
+                }
+            })
+        },
+        testGetUsers: async (parent, args, ctx, info) => {
+            return await ctx.prisma.users.findMany()
+        },
+        testCreatePost: async (parent, args, ctx) => {
+            return await ctx.prisma.posts.create({
+                data: {
+                    title: 'title134',
+                    content: 'content134',
+                }
+            })
+        },
+        testGetPosts: async (parent, args, ctx, info) => {
+            return await ctx.prisma.posts.findMany()
+        }
     }
-  }
 }
 
-const server = new GraphQLServer({ 
-    typeDefs, 
-    resolvers
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+    context: {
+        prisma
+    }
 })
 
 server.start(() => console.log('Server started on port 4000'))
